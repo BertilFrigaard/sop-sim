f6cfc1ea8c58d973d0ea954072397610e69f916d
BertilF <bertilfrigaard@gmail.com>
2025-11-18 15:35:19 +0100

    Fixed beam id generation

--- frontend/src/contexts/BeamsProvider.tsx
+++ frontend/src/contexts/BeamsProvider.tsx
@@ -6,24 +6,20 @@ export function BeamsProvider({ children }: { children: React.ReactNode }) {
     const [beams, setBeams] = useState<Beam[]>([]);
 
     const getUniqueBeamId = () => {
-        // When a beam is removed a id will be freed, this loop will therefore do the following
-        // 1. If there is no id-gap then use the id beams.length
-        //    * I do this by starting the loop at id beams.length, therefore if there is at least one
-        //    * id-gap the id beams.length will be taken
-        // 2. If there is a id-gap decrementally search till the gap is found and then return the gap-id
+        const ids = beams.map((beam) => {
+            return beam.id;
+        });
+
+        // if beams ids are still chronological then id beams.length will be free
+        // Otherwise search for the id that is unused and return that
         for (let i = beams.length; i >= 0; i--) {
-            let found = false;
-            for (let j = 0; j < beams.length; j++) {
-                if (beams[j].id == i) {
-                    found = true;
-                    continue;
-                }
-                return i;
-            }
-            if (!found) {
+            if (ids.includes(i)) {
+                continue;
+            } else {
                 return i;
             }
         }
+        // Should never happen
         throw new Error("getUniqueBeamId failed to find a new uniqe id");
     };
 
