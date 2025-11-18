d182029dbe5872f3228ac1e505201139b8290a11
BertilF <bertilfrigaard@gmail.com>
2025-11-18 15:30:31 +0100

    Added Beam Deletion - Uncovered error with id generation

--- frontend/src/components/control/BeamInspector.tsx
+++ frontend/src/components/control/BeamInspector.tsx
@@ -3,10 +3,12 @@ import { useSelection } from "../../contexts/useSelection";
 import type { Beam } from "../../types/beamTypes";
 import { BeamInput } from "./inputs/BeamInput";
 import { defaultElasticModulus, defaultInertias } from "../../lib/physics/beamDefaults";
+import { useBeams } from "../../contexts/useBeams";
 
 export function BeamInspector() {
     const [beam, setBeam] = useState<Beam | null>(null);
-    const { getSelectedBeam } = useSelection();
+    const { getSelectedBeam, setSelectedBeamId } = useSelection();
+    const { setBeams } = useBeams();
 
     useEffect(() => {
         setBeam(getSelectedBeam());
@@ -14,7 +16,22 @@ export function BeamInspector() {
 
     return (
         <div className="bg-gray-50 border-2 border-gray-200 p-5 w-full">
-            <h2 className="text-2xl font-bold">Beam Inspector (Beam ID: {beam ? beam.id : "Loading"})</h2>
+            <div className="flex justify-between">
+                <h2 className="text-2xl font-bold">Beam Inspector (Beam ID: {beam ? beam.id : "Loading"})</h2>
+                {beam && (
+                    <button
+                        className="bg-white border-2 border-gray-200 px-5 rounded-md cursor-pointer hover:scale-98"
+                        onClick={() => {
+                            setSelectedBeamId(null);
+                            setBeams((prev) => {
+                                return prev.filter((v) => v.id !== beam.id);
+                            });
+                        }}
+                    >
+                        Slet bjælke
+                    </button>
+                )}
+            </div>
             <div className="flex gap-5 pt-5 flex-wrap">
                 <BeamInput title="F | Belastning [N]" param="F" options={null} />
                 <BeamInput title="L | Længde [M]" param="L" options={null} />
